854673560aa54471a3462168e96166d62f1fc150
Kiriru Trofimov <kiriru.trofimov@gmail.com>
2020-12-27 16:43:45 +0100

    add mongodbatlas_cluster definition to atlas.tf

--- terraform/atlas.tf
+++ terraform/atlas.tf
@@ -6,6 +6,23 @@ provider "mongodbatlas" {
 
 # cluster
 
+resource "mongodbatlas_cluster" "mongo_cluster" {
+  project_id   = var.atlas_project_id
+  name         = "${var.app_name}-${terraform.workspace}"
+  num_shards   = 1
+
+  replication_factor           = 3
+  provider_backup_enabled      = true
+  auto_scaling_disk_gb_enabled = true
+  mongo_db_major_version       = "3.6"
+
+  //Provider Settings "block"
+  provider_name               = "GCP"
+  disk_size_gb                = 10
+  provider_instance_size_name = "M10"
+  provider_region_name        = "CENTRAL_US"
+}
+
 # db user
 
 # ip whitelist
